25c94e4c390084542b3bb7cc64a042b743e2b9fd
sonaric <stasketris@gmail.com>
2017-03-14 00:18:11 +0200

    Add set background from camera v3(correction)

--- app/src/main/java/com/example/stanislav/programcall/Histories.java
+++ app/src/main/java/com/example/stanislav/programcall/Histories.java
@@ -1,7 +1,10 @@
 package com.example.stanislav.programcall;
 
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.graphics.Matrix;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,35 +22,71 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
-public class Histories extends AppCompatActivity {
+public class Histories extends Activity {
 
     File directory;
     final int TYPE_PHOTO=1;
     final int REQUEST_CODE_PHOTO=1;
     ImageView ivPhoto;
+    Handler h;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_histories);
+
         createDirectory();
         ivPhoto=(ImageView)findViewById(R.id.ivPhoto);
+        progressBar=(ProgressBar)findViewById(R.id.progressBar);
+
+
         SharedPreferences sPref=getSharedPreferences( "contactTransport",MODE_PRIVATE );
         String savedText = sPref.getString( "photobackground", "" );
-        if(!savedText.equals("")){
-            Bitmap bitmap=null;
-            try {
-                bitmap=MediaStore.Images.Media.getBitmap(this.getContentResolver(),Uri.parse(savedText));
-                Matrix matrix = new Matrix();
-                matrix.postRotate(90);
-                bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
-            } catch (IOException e) {
-                e.printStackTrace();
+
+        h=new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                switch (msg.what){
+                    case 1:
+                        ivPhoto.setImageBitmap((Bitmap) msg.obj);
+                        progressBar.setVisibility(View.INVISIBLE);
+                        break;
+                }
+
             }
-            ivPhoto.setImageBitmap(bitmap);
-        }
+        };
+
+        getBackgroundBitmap(savedText,this);
+}
+
+    private void getBackgroundBitmap(final String uri, final Histories parent){
+        progressBar.setVisibility(View.VISIBLE);
+        Thread t=new Thread(new Runnable() {
+            Message msg;
+            @Override
+            public void run() {
+                Bitmap bitmap = null;
+                if(!uri.equals("")) {
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(parent.getContentResolver(), Uri.parse(uri));
+                        Matrix matrix = new Matrix();
+                        matrix.postRotate(90);
+                        bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+                        msg=h.obtainMessage(1,0,0,bitmap);
+                        h.sendMessage(msg);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        t.start();
     }
+
+
 
     public void onClickPhoto(View view){
         Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -61,16 +100,7 @@ public class Histories extends AppCompatActivity {
             if (resultCode==RESULT_OK){
                 SharedPreferences sPref=getSharedPreferences( "contactTransport",MODE_PRIVATE );
                 String savedText = sPref.getString( "photobackground", "" );
-                Bitmap bitmap=null;
-                try {
-                    bitmap=MediaStore.Images.Media.getBitmap(this.getContentResolver(),Uri.parse(savedText));
-                    Matrix matrix = new Matrix();
-                    matrix.postRotate(90);
-                    bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                ivPhoto.setImageBitmap(bitmap);
+                getBackgroundBitmap(savedText,this);
             }
         }
     }
